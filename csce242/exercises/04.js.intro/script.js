e994e91f27b50ac219206741cd8282ebf673f4b6
Adam Gerrard <agerrard@email.sc.edu>
2025-02-06 09:45:48 -0500

    added buttons and made them do stuff in class

--- csce242/exercises/04.js.intro/script.js
+++ csce242/exercises/04.js.intro/script.js
@@ -1,13 +1,26 @@
 //do stuff when button clicked
 document.getElementById("btn-click-me").onclick = () => {
     console.log("WOW!");
-    document.getElementById("result").innerHTML="Hi Portia";
+    document.getElementById("result").innerHTML="Good Times";
 };
 
 //change color
 document.getElementById("btn-color").onclick = () => {
     const messageP = document.getElementById("message");
-    messageP.innerHTML = "Good bye";
+    messageP.innerHTML = "Bad Times";
     messageP.classList.toggle("sad");
 
+}
+
+//clear the text
+document.getElementById("btn-clear-me").onclick = () => {
+    const messageC = document.getElementById("message", "result");
+    messageC.classList.remove("hidden");
+}
+
+//enter feeling
+document.getElementById("fname").onkeydown = (event) => {
+    //const emotion = document.getElementById("fname").value;
+    const emotion = event.currentTarget.value;
+    console.log(emotion);
 }
