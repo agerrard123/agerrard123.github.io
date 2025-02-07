dc39c88d1f4c8fc89acd90377610da9143e1e00e
Adam Gerrard <agerrard@email.sc.edu>
2025-02-07 12:52:55 -0500

    added color picker

--- csce242/assignments/assignment07/script.js
+++ csce242/assignments/assignment07/script.js
@@ -3,3 +3,8 @@ document.getElementById("col1of3").onclick = () => {
     console.log("WOW!");
     document.getElementById("print-hello").innerHTML="hello";
 }
+
+//change color when button is clicked
+document.getElementById("color-pick").onclick = () => {
+    console.log("WOW!");
+}
