d4e5e2d5f80709065a7d8568485aacf9b00d9036
Adam Gerrard <agerrard@email.sc.edu>
2025-02-07 18:35:02 -0500

    cant fill the star, i give up

--- csce242/assignments/assignment07/script.js
+++ csce242/assignments/assignment07/script.js
@@ -2,6 +2,13 @@
 document.getElementById("col1of3").onclick = () => {
     const message = document.getElementById("print-hello");
     message.innerHTML += "hello<br>";
-}
+};
 
 //change color when button is clicked
+const colorPicker = document.getElementById('color-pick');
+const star = document.querySelector('.star');
+
+colorPicker.addEventListener('input', (event) => {
+    star.style.borderBottomColor = event.target.value;
+    star.style.borderColor = event.target.value;
+});
