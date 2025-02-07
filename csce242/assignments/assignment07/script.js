fd9a8a266126aa5a0231ab630b2ed24da565ec21
Adam Gerrard <agerrard@email.sc.edu>
2025-02-06 21:55:36 -0500

    unsuccessful changes, no idea how to do this

--- csce242/assignments/assignment07/script.js
+++ csce242/assignments/assignment07/script.js
@@ -1,5 +1,6 @@
 //print "Say Hello" when button (col1of3) is clicked
-document.getElementById("col1of3").onclick = () => {
-    console.log("help");
-    document.getElementById("say-hello").innerHTML = "Say Hello";
-};
+const doStuff = () => {
+    console.log("doing stuff");
+}
+
+document.getElementById("btn-click-me").onclick = doStuff;
