80cdf19d6637dd833536c0fec0ca6f11b8dbd51d
Adam Gerrard <agerrard@email.sc.edu>
2025-02-13 11:13:58 -0500

    added ability to toggle back and forth between exercises

--- csce242/assignments/assignment08/script.js
+++ csce242/assignments/assignment08/script.js
@@ -1,19 +1,26 @@
-//toggleling the nav
-document.getElementById("toggle-nav").onclick = () => {
-    document.getElementById("nav-items").classList.toggle("hide-small");
+//toggleling the nav for ex1
+document.getElementById("toggle-nav-ex1").onclick = () => {
+    document.getElementById("nav-items-ex1").classList.toggle("hide-small");
+};
+
+//toggleling the nav for ex2
+document.getElementById("toggle-nav-ex2").onclick = () => {
+    document.getElementById("nav-items-ex2").classList.toggle("hide-small");
+};
+
+//clicking exercise 2
+document.getElementById("ex2-item").onclick = () => {
+    const display = document.getElementById("container1");
+    display.classList.add("hidden");
+    document.getElementById("container2").classList.remove("hidden");
 };
 
 //clicking exercise 1
-document.getElementById("ex1").onclick = () => {
-
-}
-
-//clicking exercise 2
-document.getElementById("ex2").onclick = () => {
+document.getElementById("ex1-item").onclick = () => {
     const display = document.getElementById("container1");
-    display.classList.remove("display-ex1");
-    display.classList.add("display-ex2");
-}
+    display.classList.remove("hidden");
+    document.getElementById("container2").classList.add("hidden");
+};
 
 //changing mode of transportation
 document.getElementById("txt-transportation").onkeyup = () => {
@@ -32,5 +39,4 @@ document.getElementById("txt-transportation").onkeyup = () => {
     else if(transportation == "skateboard") {
         document.getElementById("img-transportation").src = "images/skateboard.jpeg";
     }
-
-}
+};
