ff72b9f3665b00db606bebf1e15bf977ef47aea7
Adam Gerrard <agerrard@email.sc.edu>
2025-02-12 22:19:40 -0500

    completed exercise 1 and changed up the style

---
+++ csce242/assignments/assignment08/script.js
@@ -0,0 +1,24 @@
+//toggleling the nav
+document.getElementById("toggle-nav").onclick = () => {
+    document.getElementById("nav-items").classList.toggle("hide-small");
+};
+
+//changing mode of transportation
+document.getElementById("txt-transportation").onkeyup = () => {
+    const transportation = document.getElementById("txt-transportation").value;
+    document.getElementById("img-transportation").src = "";
+
+    if(transportation == "bike") {
+        document.getElementById("img-transportation").src = "images/bike.jpg";
+    }
+    else if(transportation == "scooter") {
+        document.getElementById("img-transportation").src = "images/scooter.png";
+    }
+    else if(transportation == "car") {
+        document.getElementById("img-transportation").src = "images/car.jpeg";
+    }
+    else if(transportation == "skateboard") {
+        document.getElementById("img-transportation").src = "images/skateboard.jpeg";
+    }
+
+}
