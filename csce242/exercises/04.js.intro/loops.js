37c5c665e61dfa767b5be3c71364cbe4cb42af14
Adam Gerrard <agerrard@email.sc.edu>
2025-02-18 09:35:00 -0500

    updated code in class

--- csce242/exercises/04.js.intro/loops.js
+++ csce242/exercises/04.js.intro/loops.js
@@ -1,11 +1,25 @@
 document.getElementById("btn-display").onclick = () => {
-    const startNum = document.getElementById("txt-start").value;
-    const endNum = document.getElementById("txt-end").value;
+    const startNum = parseInt(document.getElementById("txt-start").value);
+    const endNum = parseInt(document.getElementById("txt-end").value);
+    const displayArea = document.getElementById("start-end-display");
+    displayArea.innerHTML = "";
     const errorDisplay = document.getElementById("error-num-order");
     errorDisplay.innerHTML = " "; //clear out any previous errors
+    const favMessageP = document.getElementById("fav-message");
+    favMessageP.innerHTML = "";
 
-    if(endNum < startNum) {
+    if(parseInt(endNum) < parseInt(startNum)) {
         errorDisplay.innerHTML = `${endNum} is less than ${startNum}.`
         return; //dont do anything else if you already have an error
+    }
+
+    for(let i = startNum; i <= endNum; i++) {
+        const listItem = document.createElement("li");
+        listItem.innerHTML = i;
+        displayArea.append(listItem);
+        //attach event to onclick listitem
+        listItem.onclick = () => {
+            favMessageP.innerHTML = `You clicked ${i}.`
+        }
     }
 }
