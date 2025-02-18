cef8a2df76dcc1a81aeb532216d7bc72e486f6ff
Adam Gerrard <agerrard@email.sc.edu>
2025-02-18 09:10:40 -0500

    updates

---
+++ csce242/exercises/04.js.intro/loops.js
@@ -0,0 +1,11 @@
+document.getElementById("btn-display").onclick = () => {
+    const startNum = document.getElementById("txt-start").value;
+    const endNum = document.getElementById("txt-end").value;
+    const errorDisplay = document.getElementById("error-num-order");
+    errorDisplay.innerHTML = " "; //clear out any previous errors
+
+    if(endNum < startNum) {
+        errorDisplay.innerHTML = `${endNum} is less than ${startNum}.`
+        return; //dont do anything else if you already have an error
+    }
+}
