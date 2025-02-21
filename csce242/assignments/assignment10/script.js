deb8f43b9d0c10cd869d20286347721045d6f00a
Adam Gerrard <agerrard@email.sc.edu>
2025-02-20 22:13:37 -0500

    loop through items and appended to section

--- csce242/assignments/assignment10/script.js
+++ csce242/assignments/assignment10/script.js
@@ -1,9 +1,20 @@
 const items = ["Happy Birthday", "Crazy Clown", "It's Raining", "Quiet Time", "Working Hard", "Work from Home"];
+// items["Happy Birthday"] = "";
+// items["Crazy Clown"] = "";
+// items["It's Raining"] = "";
+// items["Quiet Time"] = "";
+// items["Working Hard"] = "";
+// items["Work from Home"] = "";
+const section = document.getElementById("display-list");
 const ul = document.createElement("ul");
 document.getElementById("display-list").append(ul);
 
-items.forEach((item) => {
+for(let item in items) {
     const li = document.createElement("li");
     ul.append(li);
-    li.innerHTML = (`${item}`);
-});
+    li.innerHTML = items[item];
+
+    li.onclick = () => {
+        console.log("clicked: ", items[item]);
+    }
+}
