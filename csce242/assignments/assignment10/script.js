147aa6ca168d0bb39bf9d597e8388ff0cfd5d32d
Adam Gerrard <agerrard@email.sc.edu>
2025-02-21 10:38:25 -0500

    got the popup working without the close button working

--- csce242/assignments/assignment10/script.js
+++ csce242/assignments/assignment10/script.js
@@ -1,20 +1,32 @@
-const items = ["Happy Birthday", "Crazy Clown", "It's Raining", "Quiet Time", "Working Hard", "Work from Home"];
-// items["Happy Birthday"] = "";
-// items["Crazy Clown"] = "";
-// items["It's Raining"] = "";
-// items["Quiet Time"] = "";
-// items["Working Hard"] = "";
-// items["Work from Home"] = "";
+const items = [];
+items["Happy Birthday"] = "images/birthday.jpg";
+items["Crazy Clown"] = "images/clown.jpg";
+items["It's Raining"] = "images/rain.jpg";
+items["Quiet Time"] = "images/read.jpg";
+items["Working Hard"] = "images/shovel.jpg";
+items["Work from Home"] = "images/work.jpg";
+
 const section = document.getElementById("display-list");
-const ul = document.createElement("ul");
-document.getElementById("display-list").append(ul);
+const popup = document.getElementById("popup");
+const popupTitle = document.getElementById("popup-title");
+const popupImg = document.getElementById("popup-image");
+const closeBtn = document.getElementById("close");
 
-for(let item in items) {
-    const li = document.createElement("li");
-    ul.append(li);
-    li.innerHTML = items[item];
+for(let title in items) {
+    let container = document.createElement("div");
+    container.className = "title";
+    container.innerHTML = title;
+    container.onclick = () => {
+        popupTitle.innerHTML = title;
+        popupImg.src = items[title];
+        popup.style.display = "block";
+        container.append(popup);
+        
+    };
+    section.append(container);
 
-    li.onclick = () => {
-        console.log("clicked: ", items[item]);
+    closeBtn.onclick = () => {
+        popup.style.display = "none";
     }
 }
+
